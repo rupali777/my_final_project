6822769604d6181a0124306e352d4114f58801fa
rupali777 <rupali.badgujar@os3infotech.com>
2017-10-07 17:57:18 +0530

    first

--- 4.ino
+++ 4.ino
@@ -16,7 +16,7 @@ const char* host = "10.0.31.2";
 //const char* privateKey = "....................";
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   delay(10);
 
   // We start by connecting to a WiFi network
@@ -25,16 +25,16 @@ void setup() {
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
+
   WiFi.begin(ssid, password);
-  
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
@@ -47,7 +47,7 @@ void loop() {
 
   Serial.print("connecting to ");
   Serial.println(host);
-  
+
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   const int httpPort = 80;
@@ -55,7 +55,7 @@ void loop() {
     Serial.println("connection failed");
     return;
   }
-  
+
   // We now create a URI for the request
   String url = "/api/index.php?a=res";
 //  url += streamId;
@@ -63,16 +63,16 @@ void loop() {
 //  url += privateKey;
 //  url += "&value=";
 //  url += value;
-//  
+//
   Serial.print("Requesting URL: ");
   Serial.println(url);
-  
+
   // This will send the request to the server
- 
+
    client.print("GET /api/index.php?a=res HTTP/1.1\r\n");
    client.println("Host: 10.0.31.2 \r\n");
    client.println("Connection: close\r\n\r\n");
-   
+
   unsigned long timeout = millis();
   while (client.available() == 0) {
     if (millis() - timeout > 5000) {
@@ -81,7 +81,7 @@ void loop() {
       return;
     }
   }
-//  
+//
 
 //client.print("GET / search?q=arduino HTTP/1.1\r\n");
 //client.print("host: www.google.co.in");
@@ -95,7 +95,7 @@ void loop() {
  { //headers..
 //Serial.print(".");
   if (line=="\n")
-  { //skips the empty space at the beginning 
+  { //skips the empty space at the beginning
 section="json";
   }
  }
@@ -141,6 +141,3 @@ Serial.println(res);
  Serial.println();
  Serial.println("closing connection");
  }
-
-
-
